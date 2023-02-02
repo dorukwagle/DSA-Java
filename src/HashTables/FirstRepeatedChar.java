3b4799c4666691c76b85e5054b5112166de7660a
dorukwagle <dorukwagle@gmail.com>
2023-02-02 12:09:47 +0545

    hashtables implemented

---
+++ src/HashTables/FirstRepeatedChar.java
@@ -0,0 +1,26 @@
+package HashTables;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FirstRepeatedChar {
+    public static char getFirstRepeatedChar(String str) {
+        Set<Character> set = new HashSet<>();
+        char[] charArray = str.toLowerCase().toCharArray();
+
+        for (var ch : charArray){
+            if(set.contains(ch))
+                return ch;
+            set.add(ch);
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    public static void main(String[] args) {
+        String test = "helo world";
+        System.out.println(getFirstRepeatedChar(test));
+    }
+}
---
+++ src/HashTables/HashTable.java
@@ -0,0 +1,81 @@
+package HashTables;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class HashTable {
+    private class Entry{
+        private int key;
+        private String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] array;
+
+    public HashTable(int initialCapacity){
+        array = new LinkedList[initialCapacity];
+    }
+
+    public HashTable(){
+        this(100);
+    }
+
+    public void put(int k, String v) {
+        int index = hash(k);
+        if(array[index] == null)
+            array[index] = new LinkedList<>();
+
+        var entry = getEntry(k);
+        if (entry != null)
+            entry.value = v;
+        array[index].addLast(new Entry(k, v));
+    }
+
+    public String get(int key){
+        var entry = getEntry(key);
+        if (entry != null)
+            return entry.value;
+        throw new NoSuchElementException();
+    }
+
+    public void remove(int key){
+        var list = getList(key);
+        var entry = getEntry(key);
+        if ( list != null && entry != null) {
+            list.remove(entry);
+            return;
+        }
+        throw new NoSuchElementException();
+    }
+
+    private LinkedList<Entry> getList(int key){
+        return array[hash(key)];
+    }
+
+    private Entry getEntry(int key){
+        var list = getList(key);
+        if (list != null) {
+            for (var entry : list) {
+                if (entry.key == key)
+                    return entry;
+            }
+        }
+        return null;
+    }
+
+    private boolean isNull(int index){
+        return array[index] == null;
+    }
+
+    private int hash(int key){
+        return Math.abs(key) % array.length;
+    }
+
+}
