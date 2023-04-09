e4b13382d53ccdfee7ea775f95475b364ed5a793
dorukwagle <dorukwagle@gmail.com>
2023-04-09 13:53:16 +0545

    AVLTree finding correct node to balance

--- src/AVLTrees/AVLTree.java
+++ src/AVLTrees/AVLTree.java
@@ -5,6 +5,7 @@ public class AVLTree {
         private AVLNode leftChild;
         private AVLNode rightChild;
         private int value;
+        private int height = 0;
 
         public AVLNode(int value) {
             this.value = value;
@@ -18,6 +19,8 @@ public class AVLTree {
 
     private AVLNode root;
 
+
+
     private AVLNode insert(AVLNode node, int value) {
         if(node == null)
             return new AVLNode(value);
@@ -27,11 +30,50 @@ public class AVLTree {
         else
             node.rightChild = insert(node.rightChild, value);
 
+        // calculate the node height
+        node.height = 1 + Math.max(height(node.leftChild), height(node.rightChild));
+
+        // check for balance factor
+        balance(node);
        return node;
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1: node.height;
+    }
+
+    private void balance(AVLNode node) {
+        if(isLeftHeavy(node)) {
+            // node is left heavy, right rotation required
+            if(balanceFactor(node.leftChild) < 0)
+                System.out.println(node.value + " requires left right");
+            else System.out.println(node.value + " requires right rotation");
+        }
+        if(isRightHeavy(node)){
+            if(balanceFactor(node.rightChild) > 0)
+                System.out.println(node.value + " requires right left rotation");
+                // node is right heavy, left rotation required
+            else System.out.println(node.value + " requires left rotation");
+        }
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+            return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return (node == null) ? 0 : height(node.leftChild) - height(node.rightChild);
     }
 
     public void insert(int value){
         root = insert(root, value);
     }
 
+    public int height(){
+        return height(root);
+    }
 }
--- src/AVLTrees/App.java
+++ src/AVLTrees/App.java
@@ -5,9 +5,16 @@ public class App {
         AVLTree tree = new AVLTree();
         tree.insert(34);
         tree.insert(23);
-        tree.insert(54);
-        tree.insert(89);
+        tree.insert(44);
+        tree.insert(35);
+        tree.insert(93);
         tree.insert(12);
+        tree.insert(10);
+        tree.insert(40);
+        tree.insert(30);
+        tree.insert(400);
+        tree.insert(800);
+        tree.insert(600);
         System.out.println("done");
     }
 }
