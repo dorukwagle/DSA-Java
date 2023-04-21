adc7e0a947628dd8d33dedddcf26a7561b194aa5
dorukwagle <dorukwagle@gmail.com>
2023-04-21 21:37:34 +0545

    bubble sort algorithm optimized a bit

--- src/algorithms/sortings/BubbleSort.java
+++ src/algorithms/sortings/BubbleSort.java
@@ -16,6 +16,23 @@ public class BubbleSort {
         }
     }
 
+    public static void sortOptim(int[] array) {
+        boolean isSorted = false;
+        for (int i = 0; i < array.length; i++) {
+            isSorted = true;
+            for (int j = 1; j < array.length - 1; j++) {
+                if ( array[j] < array[j - 1] ) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted)
+                return;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {9, 8, 2, 5, 7, 1, 3, 34, 22, 14};
         sort(arr);
