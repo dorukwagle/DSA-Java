a00b5858870573fc72576e2b57a83f056cfd2866
dorukwagle <dorukwagle@gmail.com>
2023-04-22 16:45:20 +0545

    MergeSort implemented

--- src/algorithms/sortings/App.java
+++ src/algorithms/sortings/App.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         int[] arr = {9, 8, 2, 5, 7, 1, 3, 34, 22, 14};
-        InsertionSort.sort(arr);
+        MergeSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
--- src/algorithms/sortings/MergeSort.java
+++ src/algorithms/sortings/MergeSort.java
@@ -2,6 +2,39 @@ package algorithms.sortings;
 
 public class MergeSort {
     public static void sort(int[] arr) {
+        int len = arr.length;
+        int middle = len / 2;
+        if (len < 2)
+            return;
 
+        int[] left = new int[middle];
+        int[] right = new int[len - middle];
+
+        for (int i = 0; i < middle; ++i)
+            left[i] = arr[i];
+
+        for (int i = middle; i < right.length + middle; ++i)
+            right[i - middle] = arr[i];
+
+        sort(left);
+        sort(right);
+
+        merge(left, right, arr);
+    }
+
+    private static void merge(int[] left, int[] right, int[] result) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                result[k++] = left[i++];
+            else result[k++] = right[j++];
+        }
+
+        while (i < left.length)
+            result[k++] = left[i++];
+
+        while (j < right.length)
+            result[k++] = right[j++];
     }
 }
